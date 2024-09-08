a7d74b101877774703a9ffe32bdef51768693669
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-09-08 21:53:06 +0800

    initialize project

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import user from "./routes/user.js";
+import errorHandler from "./middleware/error.js";
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+
+//body parser middleware
+app.use(express.json());
+//user routes
+app.use("/api/admin/", user);
+
+// Handle 404 errors for undefined routes
+app.use((req, res, next) => {
+  res.status(404).json({
+      success: false,
+      message: 'Page not found',
+  });
+});
+
+//implement error handler
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
