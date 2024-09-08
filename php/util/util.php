70ec636d0f27b3adbcc33e533aaf174b49524e67
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-09-08 19:10:34 +0800

    set up structure

---
+++ php/util/util.php
@@ -0,0 +1,15 @@
+<?php
+
+define("HR_ROLE", "1");
+define("EMPLOYEE_ROLE", "2");
+define("ACTIVE_SESSION", 1);
+
+function dd($value)
+{
+	echo "<pre>";
+
+	var_dump($value);
+
+	echo "</pre>";
+	die();
+}
