4cc21da86cd1b5c7a9b939f165060a8cb489821d
Kaarlo Sasiang <sasiang64@gmail.com>
2024-09-09 13:02:08 +0800

    removed y margin on login and signup page

--- acetrack/src/pages/LoginPage.jsx
+++ acetrack/src/pages/LoginPage.jsx
@@ -7,7 +7,7 @@ export default function Component() {
     <>
       <NavBar />
       <div className="flex items-center justify-center min-h-screen bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.1] text-black">
-        <div className="w-full max-w-lg p-8 my-4 space-y-6">
+        <div className="w-full max-w-lg p-8 space-y-6">
           <LoginForm />
         </div>
       </div>
