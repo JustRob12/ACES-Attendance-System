ef190dae3d68c8f7189bd257b2e973f9a3ca5e7b
Kaarlo Sasiang <sasiang64@gmail.com>
2024-09-09 13:07:09 +0800

    updates for paddings responsiveness on login and signup page

--- acetrack/src/pages/LoginPage.jsx
+++ acetrack/src/pages/LoginPage.jsx
@@ -6,8 +6,8 @@ export default function Component() {
   return (
     <>
       <NavBar />
-      <div className="flex items-center justify-center min-h-screen bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.1] text-black">
-        <div className="w-full max-w-lg p-8 space-y-6">
+      <div className="flex justify-center min-h-screen bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.1] text-black">
+        <div className="w-full max-w-lg p-0 md:p-8 space-y-6">
           <LoginForm />
         </div>
       </div>
