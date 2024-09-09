4ab870a355b2ad5387988d5ce421bc3fd33fd8c3
Kaarlo Sasiang <sasiang64@gmail.com>
2024-09-09 13:00:04 +0800

    updated coming soon component

--- acetrack/src/pages/ComingSoon.jsx
+++ acetrack/src/pages/ComingSoon.jsx
@@ -4,12 +4,12 @@ import acesLogo from "../assets/aces-logo.png";
 
 export default function ComingSoon() {
   return (
-    <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+    <BackgroundLines className="h-screen flex items-center justify-center w-full flex-col px-4">
       <img src={acesLogo} alt="Aces Logo" className="size-36 z-50" />
-      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-3xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
         AceTrack Coming Soon
       </h2>
-      <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+      <p className="max-w-xl mx-auto text-lg text-neutral-700 dark:text-neutral-400 text-center">
         Association Of Computing And Engineering Students
       </p>
     </BackgroundLines>
