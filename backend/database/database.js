cbdd7c9dc96d307bc2ef51131795822475670e0f
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-09-10 23:33:33 +0800

    fixed db port configuration

--- backend/database/database.js
+++ backend/database/database.js
@@ -4,7 +4,7 @@ const HOST = process.env.DB_HOST;
 const USER = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 const DATABASE = process.env.DB_NAME;
-const DP_PORT = process.env.DB_PORT;
+const DB_PORT = process.env.DB_PORT;
 
 const db = mysql.createConnection({
   host: HOST,
