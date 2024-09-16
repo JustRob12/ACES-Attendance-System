93d1685374c0f91e90ee49bcd605eb58ed62cb78
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-09-16 21:25:59 +0800

    change the cookie configuration sameSite to "none"

--- backend/controller/auth/authController.js
+++ backend/controller/auth/authController.js
@@ -54,7 +54,7 @@ export const login = async (req, res, next) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     }); // 7 days
 
